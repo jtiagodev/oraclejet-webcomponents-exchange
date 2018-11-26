0689445bfdf8dc99a94d5dda8956ef5444243cb6
Bernardo Reis <berna_reis7@hotmail.com>
2018-11-26 21:10:18 +0000

    TypeScript 0.1

--- Oracle_JET_Community_Marketplace/src/js/viewModels/dashboard.js
+++ Oracle_JET_Community_Marketplace/src/js/viewModels/dashboard.js
@@ -1,13 +1,22 @@
-define(["require", "exports", "knockout"], function (require, exports, ko) {
+define(["require", "exports", "knockout", "ojs/ojinputtext", "ojs/ojnavigationlist", "ojs/ojbutton", "ojs/ojselectcombobox"], function (require, exports, ko) {
     "use strict";
     var DashboardViewModel = /** @class */ (function () {
         function DashboardViewModel() {
             var self = this;
-            self.something = ko.observable("This parapgraph uses content from it's own 'DashboardViewModel' Class. The module is found in the /ts folder");
-            self.something2 = ko.observableArray(["Bom dia"]);
-            /*
-             * Your viewModel code goes here
-             */
+            // Texts
+            self.marketplacetitle = ko.observable("Some Component");
+            self.categoryText = ko.observable("A.I. > Chatbots");
+            self.updatedText = ko.observable("v1.0 (Update 24-05-2010)");
+            self.userText = ko.observable("Soham Saputa");
+            self.ratingText = ko.observable("3.5/5");
+            self.companieNameText = ko.observable("Capgemini Portugal");
+            self.votesText = ko.observable("24035 votes");
+            self.downloadTimesText = ko.observable("Downloaded 3425 times");
+            self.keywordsPlaceholder = ko.observable('Find a Web Component...');
+            self.downloadButtonText = ko.observable('DOWNLOAD');
+            self.searchButtonText = ko.observable('SEARCH');
+            self.keywordsValue = ko.observable();
+            self.sortValue = ko.observable();
         }
         /**
          * Optional ViewModel method invoked after the View is inserted into the
